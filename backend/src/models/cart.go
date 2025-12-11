501c735f1b7a5da074e36dfb476583af565b47e5
Kester Baur <austin.dawes@kbh.net>
2025-12-11 19:52:44 +0100

    Should be fixed.

--- backend/src/models/cart.go
+++ backend/src/models/cart.go
@@ -1,16 +1,25 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"github.com/govalues/decimal"
+	"time"
 )
 
 type CartItem struct {
+	ID       uuid.UUID `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+	ItemId   int       `gorm:"column:item_id;type:int;default:1"`
+	Quantity int       `gorm:"column:quantity;type:int;default:1"`
+	AddedAt  time.Time `gorm:"column:item_id;type:int;default:1"`
+}
+
+type ActualCartItem struct {
 	Product  Product
 	Quantity int `gorm:"type:int;default:1"`
 }
 
 // Optional aggregate, not persisted; useful for responses.
 type Cart struct {
-	Items []CartItem
+	Items []ActualCartItem
 	Total decimal.Decimal
 }
