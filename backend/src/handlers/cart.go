1c22af25798f80be60ae968c3603ea2699a4df04
Kester Baur <austin.dawes@kbh.net>
2025-12-11 19:17:06 +0100

    Updated routing to include ids.

--- backend/src/handlers/cart.go
+++ backend/src/handlers/cart.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func (dbc *DBContext) HandleGetCart(c *gin.Context) {
-
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.AbortWithError(400, fmt.Errorf("Can't convert offset to integer: %s", err))
+	}
 }
 
 func (dbc *DBContext) HandleGetCheckout(c *gin.Context) {
--- backend/src/routes/routing.go
+++ backend/src/routes/routing.go
@@ -12,10 +12,10 @@ func StartRouting(dbc *handlers.DBContext) {
 	router.GET("/products", dbc.HandleGetProducts)
 	router.GET("/products/:id", dbc.HandleGetProduct)
 
-	router.GET("/cart", dbc.HandleGetCart)
-	router.GET("/cart/checkout", dbc.HandleGetCheckout)
-	router.POST("/cart/add/:id", dbc.HandleAddCartItem)
-	router.POST("/cart/remove/:id", dbc.HandleRemoveCartItem)
+	router.GET("/cart/:id", dbc.HandleGetCart)
+	router.GET("/cart/:id/checkout", dbc.HandleGetCheckout)
+	router.POST("/cart/:id/add/:id", dbc.HandleAddCartItem)
+	router.POST("/cart/:id/remove/:id", dbc.HandleRemoveCartItem)
 
 	// Runs on 8080
 	router.Run()
