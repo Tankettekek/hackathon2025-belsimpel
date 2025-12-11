f6d66482aca3fdf1ae92cecb9de9cf790f887879
Kester Baur <austin.dawes@kbh.net>
2025-12-11 17:20:36 +0100

    Set up basic routing and added handlers for every route.

---
+++ backend/handlers/cart.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func HandleGetCart(c *gin.Context) {
+
+}
+
+func HandleGetCheckout(c *gin.Context) {
+
+}
+
+func HandleAddCartItem(c *gin.Context) {
+
+}
+
+func HandleRemoveCartItem(c *gin.Context) {
+
+}
--- backend/routes/routing.go
+++ backend/routes/routing.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"belsimel/hackathon/v2/handlers"
 	"github.com/gin-gonic/gin"
 )
 
-func startRouting() {
+func StartRouting() {
 	router := gin.Default()
+
+	router.GET("/products", handlers.HandleGetProducts)
+	router.GET("/products/:id", handlers.HandleGetProduct)
+
+	router.GET("/cart", handlers.HandleGetCart)
+	router.GET("/cart/checkout", handlers.HandleGetCheckout)
+
+	router.POST("/cart/add/:id", handlers.HandleAddCartItem)
+	router.POST("/cart/remove/:id", handlers.HandleRemoveCartItem)
 
 	// Runs on 8080
 	router.Run()
