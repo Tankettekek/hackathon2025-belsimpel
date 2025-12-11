18be091167f94a2a64f594bde678f02c9598f86d
Kester Baur <austin.dawes@kbh.net>
2025-12-11 19:46:42 +0100

    Added gorm column things for the models.

--- backend/src/models/cart.go
+++ backend/src/models/cart.go
@@ -1,17 +1,12 @@
 package models
 
 import (
-	"time"
-
-	"github.com/google/uuid"
 	"github.com/govalues/decimal"
 )
 
 type CartItem struct {
-	UserID   uuid.UUID `gorm:"type:uuid;primaryKey"`
-	ItemID   int       `gorm:"type:int;primaryKey"`
-	Quantity int       `gorm:"type:int;default:1"`
-	AddedAt  time.Time `gorm:"type:timestamp;default:current_timestamp;primaryKey"`
+	Product  Product
+	Quantity int `gorm:"type:int;default:1"`
 }
 
 // Optional aggregate, not persisted; useful for responses.
--- backend/src/models/user.go
+++ backend/src/models/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Name         string     `gorm:"type:text;not null"`
-	Email        string     `gorm:"type:text;unique;not null"`
-	PasswordHash string     `gorm:"type:text"`
-	CreatedAt    time.Time  `gorm:"type:timestamp;default:current_timestamp"`
-	CartID       *uuid.UUID `gorm:"type:uuid"`
+	ID           uuid.UUID  `gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name         string     `gorm:"column:name;type:text;not null"`
+	Email        string     `gorm:"column:email;type:text;unique;not null"`
+	PasswordHash string     `gorm:"column:password_hash;type:text"`
+	CreatedAt    time.Time  `gorm:"column:created_at;type:timestamp;default:current_timestamp"`
+	CartID       *uuid.UUID `gorm:"column:cart_id;type:uuid"`
 }
